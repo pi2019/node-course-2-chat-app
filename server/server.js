8ed034e45a72b8e1f85cd7785c89e55c3ba5ecd6
Pesh Irani <pesh@Peshs-MacBook-Pro.local>
2018-08-03 16:36:12 -0400

    Add custom emit emit events to our file

--- server/server.js
+++ server/server.js
@@ -9,19 +9,45 @@ const express = require("express"),
       io = socketIO(server);
 
 
-
+let numberOfUsers = 0;
 
 app.use(express.static(pathName));
 
 io.on("connection", function (socket) {
     console.log("new user connected");
+    numberOfUsers++;
+    console.log(`currently there are ${numberOfUsers} users`);
 
-    socket.on("disconnect", ()=>{
+    socket.on("disconnect", function () {
         console.log("User disconnected from server");
-    })
+        numberOfUsers--;
+        console.log(`currently there are ${numberOfUsers} users`);
+    });
+
+    socket.emit("newEmail", {
+        "from": "peshotan irani",
+        subject: "cool bro"
+    });
+
+    socket.on("createEmail", (substance)=>{
+        console.log(`currently ${substance.name} just joined`);
+    });
+
+    socket.emit("newMessageEvent", {
+        from: "Peshotan Irani",
+        text: "good morning folks, how is everyone???",
+        createdAt: Date.now().toLocaleString()
+        }
+    );
+
+    socket.on("createMessageEvent", (message)=>{
+        console.log(`${message.from} + "says " + ${message.text}` );
+
+        socket.emit("newMessageEvent", message) // this is to emit a message from one user to all users
+    });
 });
 
-server.listen(http_PORT, ()=>{
+server.listen(http_PORT, function() {
     console.log(`Server is up on ${http_PORT}`)
 });
 
